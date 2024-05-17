fd576461657111182c47f35fcbe6bc9b939db188
Neeraj Dalal <admin@nrjdalal.com>
2024-05-18 03:14:05 +0530

    fix: minor ui updates

--- apps/website/src/components/readrave/breadcrumb.tsx
+++ apps/website/src/components/readrave/breadcrumb.tsx
@@ -15,8 +15,15 @@ export const ReadraveBreadcrumb = ({
 
   return (
     <nav className="mb-6 mt-3 flex items-center space-x-2 text-sm text-primary/50 md:mt-0">
-      <Link className="hover:underline" href="/">
-        home
+      <Link href="/">
+        <svg
+          className="size-4 hover:text-primary"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path d="M12.97 2.59a1.5 1.5 0 0 0-1.94 0l-7.5 6.363A1.5 1.5 0 0 0 3 10.097V19.5A1.5 1.5 0 0 0 4.5 21h4.75a.75.75 0 0 0 .75-.75V14h4v6.25c0 .414.336.75.75.75h4.75a1.5 1.5 0 0 0 1.5-1.5v-9.403a1.5 1.5 0 0 0-.53-1.144l-7.5-6.363Z" />
+        </svg>
       </Link>
 
       {paths.map((path, index) => {
@@ -25,7 +32,7 @@ export const ReadraveBreadcrumb = ({
           <div className="flex items-center space-x-2" key={index}>
             <span className="select-none">/</span>
             {sidebarConfig.find((item) => item.href === href) ? (
-              <Link href={href} className="hover:underline">
+              <Link href={href} className="hover:text-primary hover:underline">
                 {path}
               </Link>
             ) : (
