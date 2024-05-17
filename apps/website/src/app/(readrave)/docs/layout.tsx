1962132774ec5205b149b4bb316490f357c598be
Neeraj Dalal <admin@nrjdalal.com>
2024-05-17 23:45:14 +0530

    fix: minor ui fixes

--- apps/website/src/app/(readrave)/docs/layout.tsx
+++ apps/website/src/app/(readrave)/docs/layout.tsx
@@ -7,10 +7,10 @@ import { sidebarConfig } from './sidebar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto max-w-screen-xl px-5 pb-20 mt-14 md:grid md:grid-cols-[200px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:px-7">
+    <div className="mx-auto max-w-screen-xl px-5 md:grid md:grid-cols-[200px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:px-7 min-h-dvh">
       <ReadraveMobilebar sidebarConfig={sidebarConfig} />
       <ReadraveSidebar sidebarConfig={sidebarConfig} />
-      <main className="pt-6">
+      <main className="py-20">
         <ReadraveBreadcrumb sidebarConfig={sidebarConfig} />
         <article className="prose max-w-none dark:prose-invert break-words">
           {children}
