cd187e3a7813a3c7e9c63e596bb5fc56450c3e14
Neeraj Dalal <admin@nrjdalal.com>
2024-05-18 01:18:55 +0530

    feat: added font and github

--- apps/website/tailwind.config.ts
+++ apps/website/tailwind.config.ts
@@ -18,6 +18,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)'],
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
