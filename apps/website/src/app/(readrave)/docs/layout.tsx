78974a3c01fd366398abb14b9fc4c32432907d1d
Neeraj Dalal <admin@nrjdalal.com>
2024-05-18 10:10:16 +0530

    refactor: merge sidebar and mobilebar

--- apps/website/src/app/(readrave)/docs/layout.tsx
+++ apps/website/src/app/(readrave)/docs/layout.tsx
@@ -2,14 +2,12 @@
 
 import { ReadraveBacknext } from '@/components/readrave/backnext'
 import { ReadraveBreadcrumb } from '@/components/readrave/breadcrumb'
-import { ReadraveMobilebar } from '@/components/readrave/mobilebar'
 import { ReadraveSidebar } from '@/components/readrave/sidebar'
 import { sidebarConfig } from './sidebar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="mx-auto min-h-dvh max-w-screen-xl px-5 md:grid md:grid-cols-[200px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:px-7">
-      <ReadraveMobilebar sidebarConfig={sidebarConfig} />
       <ReadraveSidebar sidebarConfig={sidebarConfig} />
       <main className="py-20">
         <ReadraveBreadcrumb sidebarConfig={sidebarConfig} />
--- apps/website/src/components/readrave/sidebar.tsx
+++ apps/website/src/components/readrave/sidebar.tsx
@@ -3,53 +3,129 @@
 'use client'
 
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useState } from 'react'
 
 export const ReadraveSidebar = ({
   sidebarConfig,
 }: {
   sidebarConfig: { title: string; href?: string; css?: any }[]
 }) => {
+  const [open, setOpen] = useState(false)
   const pathname = usePathname()
 
   return (
-    <aside className="top-14 z-10 hidden h-[calc(100dvh-3.5rem)] md:sticky md:block">
-      <ScrollArea className="h-full py-6">
-        <nav className="flex flex-col space-y-2.5 text-sm text-foreground/50">
-          {sidebarConfig.map((item, i) => (
-            <span key={i}>
-              {item.href ? (
-                <Link
-                  href={item.href}
-                  className={cn(
-                    'font-foreground/50 hover:text-primary hover:underline',
-                    pathname === item.href && 'text-primary',
+    <>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger className="fixed right-5 top-20 flex items-center gap-x-2 rounded-md border bg-background p-2 px-4 text-foreground md:hidden">
+          <svg
+            strokeWidth="1.5"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 rotate-180"
+          >
+            <path
+              d="M3 5H11"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+            <path
+              d="M3 12H16"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+            <path
+              d="M3 19H21"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+          </svg>
+          <span className="sr-only">Toggle Menu</span>
+        </SheetTrigger>
+        <SheetContent className="w-72 md:hidden">
+          <ScrollArea className="h-full">
+            <nav className="flex flex-col space-y-2.5 text-sm text-foreground/50">
+              {sidebarConfig.map((item, i) => (
+                <span key={i}>
+                  {item.href ? (
+                    <Link
+                      href={item.href}
+                      className={cn(
+                        'font-foreground/50 hover:text-primary hover:underline',
+                        pathname === item.href && 'text-primary',
+                      )}
+                      style={item.css}
+                      onClick={() => setOpen(false)}
+                    >
+                      <span
+                        className={cn(
+                          'hidden',
+                          pathname === item.href &&
+                            'mb-px mr-1.5 inline-block h-2 w-2 rounded-full bg-green-600 dark:bg-green-500',
+                        )}
+                      />
+                      {item.title}
+                    </Link>
+                  ) : (
+                    <p
+                      className="select-none font-semibold text-foreground/75"
+                      style={item.css}
+                    >
+                      {item.title}
+                    </p>
                   )}
-                  style={item.css}
-                >
-                  <span
+                </span>
+              ))}
+            </nav>
+          </ScrollArea>
+        </SheetContent>
+      </Sheet>
+      <aside className="top-14 z-10 hidden h-[calc(100dvh-3.5rem)] md:sticky md:block">
+        <ScrollArea className="h-full py-6">
+          <nav className="flex flex-col space-y-2.5 text-sm text-foreground/50">
+            {sidebarConfig.map((item, i) => (
+              <span key={i}>
+                {item.href ? (
+                  <Link
+                    href={item.href}
                     className={cn(
-                      'hidden',
-                      pathname === item.href &&
-                        'mb-px mr-1.5 inline-block h-2 w-2 rounded-full bg-green-600 dark:bg-green-500',
+                      'font-foreground/50 hover:text-primary hover:underline',
+                      pathname === item.href && 'text-primary',
                     )}
-                  />
-                  {item.title}
-                </Link>
-              ) : (
-                <p
-                  className="select-none font-semibold text-foreground/75"
-                  style={item.css}
-                >
-                  {item.title}
-                </p>
-              )}
-            </span>
-          ))}
-        </nav>
-      </ScrollArea>
-    </aside>
+                    style={item.css}
+                  >
+                    <span
+                      className={cn(
+                        'hidden',
+                        pathname === item.href &&
+                          'mb-px mr-1.5 inline-block h-2 w-2 rounded-full bg-green-600 dark:bg-green-500',
+                      )}
+                    />
+                    {item.title}
+                  </Link>
+                ) : (
+                  <p
+                    className="select-none font-semibold text-foreground/75"
+                    style={item.css}
+                  >
+                    {item.title}
+                  </p>
+                )}
+              </span>
+            ))}
+          </nav>
+        </ScrollArea>
+      </aside>
+    </>
   )
 }
