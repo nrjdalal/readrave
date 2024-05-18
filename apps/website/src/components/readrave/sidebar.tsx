fed6cb53f684c0574c174afc211611f293eff3e7
Neeraj Dalal <admin@nrjdalal.com>
2024-05-18 10:26:39 +0530

    fix: minor ui updates

--- apps/website/src/components/readrave/sidebar.tsx
+++ apps/website/src/components/readrave/sidebar.tsx
@@ -7,7 +7,7 @@ import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 export const ReadraveSidebar = ({
   sidebarConfig,
@@ -17,8 +17,48 @@ export const ReadraveSidebar = ({
   const [open, setOpen] = useState(false)
   const pathname = usePathname()
 
+  const NavLogic = () => {
+    return (
+      <ScrollArea className="h-full py-6">
+        <nav className="flex flex-col space-y-2.5 text-sm text-foreground/50">
+          {sidebarConfig.map((item, i) => (
+            <span key={i}>
+              {item.href ? (
+                <Link
+                  href={item.href}
+                  className={cn(
+                    'font-foreground/50 hover:text-primary hover:underline',
+                    pathname === item.href && 'text-primary',
+                  )}
+                  style={item.css}
+                  onClick={() => setOpen(false)}
+                >
+                  <span
+                    className={cn(
+                      'hidden',
+                      pathname === item.href &&
+                        'mb-px mr-1.5 inline-block h-2 w-2 rounded-full bg-green-600 dark:bg-green-500',
+                    )}
+                  />
+                  {item.title}
+                </Link>
+              ) : (
+                <p
+                  className="select-none font-semibold text-primary"
+                  style={item.css}
+                >
+                  {item.title}
+                </p>
+              )}
+            </span>
+          ))}
+        </nav>
+      </ScrollArea>
+    )
+  }
+
   return (
-    <>
+    <React.Fragment>
       <Sheet open={open} onOpenChange={setOpen}>
         <SheetTrigger className="fixed right-5 top-20 flex items-center gap-x-2 rounded-md border bg-background p-2 px-4 text-foreground md:hidden">
           <svg
@@ -53,79 +93,12 @@ export const ReadraveSidebar = ({
           <span className="sr-only">Toggle Menu</span>
         </SheetTrigger>
         <SheetContent className="w-72 md:hidden">
-          <ScrollArea className="h-full">
-            <nav className="flex flex-col space-y-2.5 text-sm text-foreground/50">
-              {sidebarConfig.map((item, i) => (
-                <span key={i}>
-                  {item.href ? (
-                    <Link
-                      href={item.href}
-                      className={cn(
-                        'font-foreground/50 hover:text-primary hover:underline',
-                        pathname === item.href && 'text-primary',
-                      )}
-                      style={item.css}
-                      onClick={() => setOpen(false)}
-                    >
-                      <span
-                        className={cn(
-                          'hidden',
-                          pathname === item.href &&
-                            'mb-px mr-1.5 inline-block h-2 w-2 rounded-full bg-green-600 dark:bg-green-500',
-                        )}
-                      />
-                      {item.title}
-                    </Link>
-                  ) : (
-                    <p
-                      className="select-none font-semibold text-foreground/75"
-                      style={item.css}
-                    >
-                      {item.title}
-                    </p>
-                  )}
-                </span>
-              ))}
-            </nav>
-          </ScrollArea>
+          <NavLogic />
         </SheetContent>
       </Sheet>
       <aside className="top-14 z-10 hidden h-[calc(100dvh-3.5rem)] md:sticky md:block">
-        <ScrollArea className="h-full py-6">
-          <nav className="flex flex-col space-y-2.5 text-sm text-foreground/50">
-            {sidebarConfig.map((item, i) => (
-              <span key={i}>
-                {item.href ? (
-                  <Link
-                    href={item.href}
-                    className={cn(
-                      'font-foreground/50 hover:text-primary hover:underline',
-                      pathname === item.href && 'text-primary',
-                    )}
-                    style={item.css}
-                  >
-                    <span
-                      className={cn(
-                        'hidden',
-                        pathname === item.href &&
-                          'mb-px mr-1.5 inline-block h-2 w-2 rounded-full bg-green-600 dark:bg-green-500',
-                      )}
-                    />
-                    {item.title}
-                  </Link>
-                ) : (
-                  <p
-                    className="select-none font-semibold text-foreground/75"
-                    style={item.css}
-                  >
-                    {item.title}
-                  </p>
-                )}
-              </span>
-            ))}
-          </nav>
-        </ScrollArea>
+        <NavLogic />
       </aside>
-    </>
+    </React.Fragment>
   )
 }
